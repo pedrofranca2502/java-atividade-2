7b908b7b6f5b35fc355e0c2368e787417e118062
pedrofranca2502 <pedrofranca250204@gmail.com>
2022-03-24 19:18:51 -0300

    codigo completo

---
+++ java.js
@@ -0,0 +1,15 @@
+function clicou1(){
+let registro1= document.getElementById("botao1")
+registro1.innerHTML= "<p>voce concluiu o primeiro ato</P>"}
+
+function clicou2(){
+let registro2= document.getElementById("botao2")
+registro2.innerHTML= "<p>voce concluiu o segundo ato</P>"}
+
+function clicou3(){
+let registro3= document.getElementById("botao3")
+registro3.innerHTML= "<p>voce concluiu o terceiro ato</P>"}
+
+function clicou4(){
+let registro4= document.getElementById("botao4")
+registro4.innerHTML= "<p>voce concluiu o quarto ato</P>"}
